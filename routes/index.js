2c56b19dc391df04ecc95735f8023b3bdfd0d40d
Laetitia Langlois <weblaetitia@hotmail.com>
2020-06-29 15:06:18 +0200

    nodyfy webpages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+// my city-list
+
+var cityList = [
+
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login');
+});
+
+/* GET weather page. */
+router.get('/weather', function(req, res, next) {
+  res.render('weather', {cityList:cityList});
+});
+
+/* POST newcity page. */
+router.post('/addcity', function(req, res, next) {
+  if (cityList < 1) {
+    cityList.push({
+      name : req.body.cityname,
+      image: './images/picto-1.png',
+      minTemp: '18.2',
+      maxTemp: '26.5',
+    })
+  } else {
+    var cityExist = false
+    cityList.forEach(city => {
+      if ((city.name != req.body.cityname) && (cityExist == false)) {
+        cityList.push({
+          name : req.body.cityname,
+          image: './images/picto-1.png',
+          minTemp: '18.2',
+          maxTemp: '26.5',
+        })
+      } else {
+        cityExist = true
+        res.locals.exist = 'City allready added';
+      }
+    })
+  }
+  res.render('weather', {cityList:cityList});
+});
+
+/* GET deletecity page. */
+router.get('/deletecity', function(req, res, next) {
+  cityList.splice(req.query.order, 1)
+  res.render('weather', {cityList:cityList});
 });
 
 module.exports = router;
