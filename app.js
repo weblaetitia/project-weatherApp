161cebcbf6a62cd85de15818c988bf9443652ea2
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-02 11:00:50 +0200

    Set up MVC structure

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
+// routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var weatherRouter = require('./routes/weather');
 
-var app = express();
+// models
+require('./models/db')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// router familys
 app.use('/', indexRouter);
+app.use('/weather', weatherRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/weather.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var router = express.Router();
+var request = require('sync-request');
+
+var CityModel = require('../models/weather')
+
+/* GET weather page. */
+router.get('/', async function(req, res, next) {
+  cityList = await CityModel.find()
+  res.render('weather', {cityList:cityList});
+});
+
+/* POST newcity page. */
+router.post('/add-city', async function(req, res) {
+  //  appeler weather api
+  var myRequest = request('GET', (`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityname}&units=metric&lang=fr&appid=3a50b49408c422fcd643322fc5c918c9`))
+  if (myRequest.statusCode > 300) {
+    res.locals.notFound = 'City not found'
+  } else {
+    var myRequest = JSON.parse(myRequest.getBody())
+    // verif si une entré existe
+    var cityExist = await CityModel.findOne({name: myRequest.name})
+
+    if (cityExist) {
+      res.locals.exist = "This city is allready in your list"
+    }
+    
+    if ((cityExist == null) && (myRequest.name)) {
+      var newCity = new CityModel ({
+        name : myRequest.name,
+        description : myRequest.weather[0].description,
+        icon : myRequest.weather[0].icon,
+        minTemp: myRequest.main.temp_min,
+        maxTemp: myRequest.main.temp_max,
+      })
+      await newCity.save()
+    } 
+  }
+  cityList = await CityModel.find()
+  res.render('weather', {cityList:cityList});
+})
+  
+
+/* GET deletecity page. */
+router.get('/delete-city', async function(req, res) {
+  // recupérer le document à suprimer
+  var cityID = req.query.id
+  // le suprimer
+  await CityModel.deleteOne( {_id:cityID})
+  // rendre la page
+  cityList = await CityModel.find()
+  res.render('weather', {cityList:cityList});
+});
+
+/* GET update-city page. */
+router.get('/update-city', async function(req, res) {
+  var cityList = await CityModel.find()
+  
+  for (i=0; i<cityList.length; i++) {
+    var myRequest = request('GET', (`https://api.openweathermap.org/data/2.5/weather?q=${cityList[i].name}&units=metric&lang=fr&appid=3a50b49408c422fcd643322fc5c918c9`))
+    // console.log(cityList[i].name)
+    myRequest = JSON.parse(myRequest.getBody())
+    // console.log(myRequest)
+    await CityModel.updateOne( 
+      { _id : cityList[i].id },
+      {
+        description : myRequest.weather[0].description,
+        icon : myRequest.weather[0].icon,
+        minTemp: myRequest.main.temp_min,
+        maxTemp: myRequest.main.temp_max, 
+      })
+  }
+  
+  // rendre la page
+  cityList = await CityModel.find()
+  res.render('weather', {cityList:cityList});
+});
+
+
+
+
+
+
+// keep this at the end
+module.exports = router;
